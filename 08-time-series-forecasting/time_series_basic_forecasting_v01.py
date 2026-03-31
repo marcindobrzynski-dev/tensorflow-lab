6b0b4657b228b8f49a28f6b5eee84c59edd80b42
Marcin Dobrzyński <dobrzynskim.developer@protonmail.com>
2026-03-31 20:15:10 +0200

    feat: add basic time-series forecasting demo

---
+++ 08-time-series-forecasting/time_series_basic_forecasting_v01.py
@@ -0,0 +1,112 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import numpy as np
+
+def plot_series(time, series, format="-", start=0, end=None):
+  plt.plot(time[start:end], series[start:end], format)
+  plt.xlabel("Time")
+  plt.ylabel("Value")
+  plt.grid(True)
+
+def trend(time, slope=0):
+  return slope * time
+
+def seasonal_pattern(season_time):
+  return np.where(
+      season_time < 0.4,
+      np.cos(season_time * 2 * np.pi),
+      1 / np.exp(3 * season_time)
+  )
+
+def seasonality(time, period, amplitude=1, phase=0):
+  season_time = ((time + phase) % period) / period
+
+  return amplitude * seasonal_pattern(season_time)
+
+def noise(time, noise_level=1, seed=None):
+  rnd = np.random.RandomState(seed)
+
+  return rnd.randn(len(time)) * noise_level
+
+def moving_average_forecast(series, window_size):
+  forecast = []
+
+  for time in range(len(series) - window_size):
+    forecast.append(series[time:time + window_size].mean())
+
+  return np.array(forecast)
+
+time = np.arange(4 * 365 + 1, dtype="float32")
+baseline = 10
+series = trend(time, .05)
+amplitude = 15
+slope = 0.09
+noise_level = 6
+
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude) + noise(time, noise_level, seed=42)
+
+plt.figure(figsize=(10, 6))
+plot_series(time, series)
+plt.show()
+
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+plt.figure(figsize=(10, 6))
+plot_series(time_train, x_train)
+plt.show()
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plt.show()
+
+naive_forecast = series[split_time -1 : -1]
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, naive_forecast)
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid, start=0, end=150)
+plot_series(time_valid, naive_forecast, start=1, end=151)
+
+print(np.mean((x_valid - naive_forecast) ** 2)) # MSE
+print(np.mean(np.abs(x_valid - naive_forecast))) # MAE
+
+# Moving average
+moving_avg = moving_average_forecast(series, 30)[split_time - 30:]
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, moving_avg)
+
+print(np.mean((x_valid - moving_avg) ** 2)) # MSE
+print(np.mean(np.abs(x_valid - moving_avg))) # MAE
+
+# Differentiation
+diff_series = (series[365:] - series[:-365])
+diff_time = time[365:]
+
+diff_moving_avg = moving_average_forecast(diff_series, 50)[split_time - 365 - 50:]
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, diff_series[split_time - 365:])
+plot_series(time_valid, diff_moving_avg)
+plt.show()
+
+diff_moving_avg_plus_past = series[split_time - 365:-365] + diff_moving_avg
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, diff_moving_avg_plus_past)
+plt.show()
+
+diff_moving_avg_plus_smooth_past = moving_average_forecast(series[split_time - 370:-360], 10) + diff_moving_avg
+
+plt.figure(figsize=(10, 6))
+plot_series(time_valid, x_valid)
+plot_series(time_valid, diff_moving_avg_plus_smooth_past)
+plt.show()
